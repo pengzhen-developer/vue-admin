94197e842b3df9dea9acdab06778fa5f617ea422
彭真 <pengzhen@medto.cn>
2018-08-17 11:42:03 +0800

    1. 引入 global.css， normal.css

--- src/main.js
+++ src/main.js
@@ -8,12 +8,28 @@ import ElUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElUI)
 
+import './assets/css/global.css'
+
+global.kindo = global.kindo || {}
+window.kindo = window.kindo || {}
+
 // 挂载到 Vue 原型
 Vue.prototype.$http = axios
+Vue.prototype.kindo = global.kindo || window.kindo
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+Vue.prototype.$http
+  .create()
+  .get(`${process.env.BASE_URL}static/config/config.json`)
+  .then(res => {
+    kindo.config = res.data
+    // document title
+    document.title = kindo.config.theme.title
+  })
+  .then(() => {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  })
